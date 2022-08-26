4f09ba7e1642f90e388d59675b0ecf10bfd83742
Alex Law <pokemon999101@hotmail.co.uk>
2022-08-26 19:01:18 +0100

    🛠️ Route examples

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * First post to this url with body like { "token_name": "yerd" }
+ */
+Route::post('/tokens/create', function (Request $request) {
+    $user = App\Models\User::first();
+    auth()->login($user);
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
+/**
+ * And then get request here with bearer token
+ */
+Route::get('/me', function () {
+    return auth()->user();
+})->middleware('auth:sanctum');
